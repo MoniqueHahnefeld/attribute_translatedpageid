14bf473e02df9b0399398fa712c4c8ff51f2e7e2
Monique Hahnefeld <info@monique-hahnefeld.de>
2015-11-01 22:47:15 +0100

    Update tl_metamodel_rendersetting.php

--- contao/dca/tl_metamodel_rendersetting.php
+++ contao/dca/tl_metamodel_rendersetting.php
@@ -17,4 +17,4 @@
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['translatedurl extends url'] = array();
+$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['translatedurl extends pageid'] = array();
